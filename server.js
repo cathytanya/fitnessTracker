f1539900049a83082c821b98806ebe9142d3fd00
cathytanya <cathycolette.tanya@gmail.com>
2021-11-11 21:25:17 -0500

    list libraries in server,js

---
+++ server.js
@@ -0,0 +1,8 @@
+// list of libraries that need to be installed
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
