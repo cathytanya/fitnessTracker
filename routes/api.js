976a28e3ad2a67f4203863f61537fbba0fd3b3bc
cathytanya <cathycolette.tanya@gmail.com>
2021-11-12 00:12:03 -0500

    put route to add exercise

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ router.get("/api/workouts", (req,res)=>{
     db.FitnessTracker.find({}).then(dbFitnessTracker =>{
         // forEach method is applied 
         dbFitnessTracker.forEach(fitness =>{
-            
+
             const total = 0;
             fitness.exercise.forEach(e => {
                 total += e.duration;
@@ -18,5 +18,22 @@ router.get("/api/workouts", (req,res)=>{
         res.json(dbFitnessTracker);
     }).catch(err =>{
         res.json(err)
+    })
+})
+
+// PUT ROUTES to ADD EXERCISE
+router.put("/api/workout/:id", (req,res)=>{
+    db.FitnessTracker.findOneAndUpdate(
+        {_id: req.params.id},
+        {
+            $inc:{totalDuration: req.body.duration},
+            $push:{exercise: req.body}
+
+        },
+        {new: true}
+    ).then(dbFitnessTracker =>{
+        res.json(dbFitnessTracker)
+    }).catch(err=>{
+        res.json(err);
     })
 })
