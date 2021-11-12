ef8c6ee8f9644a00b2eba10ba4aa38893ac7b21c
cathytanya <cathycolette.tanya@gmail.com>
2021-11-11 21:27:34 -0500

    add comment above the port variable in server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const app = express();
+// HEROKU PORT
 const PORT = process.env.PORT || 3000;
 
 
