f996d74a0ae783b4c37a37cf4edbc53b2a808cf4
cathytanya <cathycolette.tanya@gmail.com>
2021-11-12 00:29:47 -0500

    formed routes in the html.js

---
+++ routes/html.js
@@ -0,0 +1,18 @@
+// modules needed to install
+const router = require("express").Router();
+const path = require("path");
+
+// goes to the main page
+router.get("/", (req,res)=>{
+    res.sendFile(path.join(__dirname,"../public/index.html"))
+})
+
+//takes the user to teh exercise.html
+router.get("/exercise",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../public/exercise.html"))
+})
+
+//takes the user to teh stats.html
+router.get("/stats",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../public/stats.html"))
+})
