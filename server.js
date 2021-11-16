cb0cf0f856013e733f0ee591239ab2baf7d41f4b
cathytanya <cathycolette.tanya@gmail.com>
2021-11-15 22:08:40 -0500

    add to the mongoose .connect

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/fitnesstrackerdb",
      {
         useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,  
         useFindAndModify: false
     }
 );
